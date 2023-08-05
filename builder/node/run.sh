d7ff09bd9b8d2e1f6bf35c66333a69cfc432bb08
shaowenchen <mail@chenshaowen.com>
2023-08-06 06:19:18 +0800

    improvement: builder image need builder dir

--- builder/node/run.sh
+++ builder/node/run.sh
@@ -5,20 +5,21 @@ do
     echo $line
     docker buildx build --push --platform=linux/arm,linux/arm64,linux/amd64 -t hubimage/builder-node:$line - << EOF
 FROM hubimage/runtime-node:$line
-RUN npm config set registry http://registry.npm.taobao.org/ && \
+RUN mkdir -p builder && \
+    npm config set registry http://registry.npm.taobao.org/ && \
     npm config set sass_binary_site http://cdn.npm.taobao.org/dist/node-sass && \
     npm config set phantomjs_cdnurl https://cdn.npm.taobao.org/dist/phantomjs/ && \
     npm config set chromedriver_cdnurl https://cdn.npm.taobao.org/dist/chromedriver && \
     npm config set operadriver_cdnurl https://cdn.npm.taobao.org/dist/operadriver && \
     npm config set fse_binary_host_mirror https://cdn.npm.taobao.org/dist/fsevents && \
-    npm config set prefer-offline true || true
-
-RUN yarn config set registry http://registry.npm.taobao.org/ && \
+    npm config set prefer-offline true || true && \
+    yarn config set registry http://registry.npm.taobao.org/ && \
     yarn config set sass_binary_site http://cdn.npm.taobao.org/dist/node-sass && \
     yarn config set phantomjs_cdnurl https://cdn.npm.taobao.org/dist/phantomjs/ && \
     yarn config set chromedriver_cdnurl https://cdn.npm.taobao.org/dist/chromedriver && \
     yarn config set operadriver_cdnurl https://cdn.npm.taobao.org/dist/operadriver && \
     yarn config set fse_binary_host_mirror https://cdn.npm.taobao.org/dist/fsevents && \
     yarn config set install.prefer-offline true || true
+WORKDIR /builder
 EOF
 done
--- builder/python/run.sh
+++ builder/python/run.sh
@@ -5,8 +5,10 @@ do
     echo $line
     docker buildx build --push --platform=linux/arm,linux/arm64,linux/amd64 -t hubimage/builder-python:$line - << EOF
 FROM hubimage/runtime-python:$line
-RUN pip config --user set global.index https://mirrors.aliyun.com/pypi/simple/ && \
+RUN mkdir -p /builder && \
+    pip config --user set global.index https://mirrors.aliyun.com/pypi/simple/ && \
     pip config --user set global.index-url https://mirrors.aliyun.com/pypi/simple/ && \
     pip config --user set global.trusted-host mirrors.aliyun.com
+WORKDIR /builder
 EOF
 done
