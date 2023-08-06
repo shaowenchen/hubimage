bab554c35f905bdd23613e58184a121fb26f5585
shaowenchen <mail@chenshaowen.com>
2023-08-06 11:37:33 +0800

    minor: sort tag list

--- runtime/openresty/run.sh
+++ runtime/openresty/run.sh
@@ -3,7 +3,27 @@
 for line in $(cat tag)
 do
     echo $line
-    docker buildx build --load --push --platform=linux/arm,linux/arm64,linux/amd64 -t hubimage/runtime-openresty:$line - << EOF
+    docker buildx build --push --platform=linux/arm -t hubimage/runtime-openresty:$line - << EOF
+FROM openresty/openresty:$line
+LABEL maintainer="shaowenchen <mail@chenshaowen.com>"
+RUN mkdir -p /runtime && \
+    ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime && \
+    echo "Asia/Shanghai" > /etc/timezone
+WORKDIR /runtime
+EOF
+
+    echo $line
+    docker buildx build --push --platform=linux/arm64 -t hubimage/runtime-openresty:$line - << EOF
+FROM openresty/openresty:$line
+LABEL maintainer="shaowenchen <mail@chenshaowen.com>"
+RUN mkdir -p /runtime && \
+    ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime && \
+    echo "Asia/Shanghai" > /etc/timezone
+WORKDIR /runtime
+EOF
+
+    echo $line
+    docker buildx build --push --platform=linux/amd64 -t hubimage/runtime-openresty:$line - << EOF
 FROM openresty/openresty:$line
 LABEL maintainer="shaowenchen <mail@chenshaowen.com>"
 RUN mkdir -p /runtime && \
