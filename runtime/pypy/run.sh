f8f24f195c26d148f5a6b06e7f9e9e7083bd5606
shaowenchen <mail@chenshaowen.com>
2023-08-08 11:04:42 +0800

    add builder pypy

--- runtime/pypy/run.sh
+++ runtime/pypy/run.sh
@@ -3,8 +3,7 @@
 for line in $(cat tag)
 do
     echo $line
-    # docker buildx build --push --platform=linux/arm,linux/arm64,linux/amd64 -t hubimage/runtime-pypy:$line - << EOF
-    docker build --push -t hubimage/runtime-pypy:$line - << EOF
+    docker buildx build --push --platform=linux/arm,linux/arm64,linux/amd64 -t hubimage/runtime-pypy:$line - << EOF
 FROM pypy:$line
 LABEL maintainer="shaowenchen <mail@chenshaowen.com>"
 RUN mkdir -p /runtime && \
