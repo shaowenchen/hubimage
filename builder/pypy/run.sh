f1d0b098182a3a77033fdc8e8763f2337ab96aec
shaowenchen <mail@chenshaowen.com>
2023-08-08 11:24:28 +0800

    remove pypy arm

--- builder/pypy/run.sh
+++ builder/pypy/run.sh
@@ -3,7 +3,7 @@
 for line in $(cat tag)
 do
     echo $line
-    docker buildx build --push --platform=linux/arm,linux/arm64,linux/amd64 -t hubimage/builder-pypy:$line - << EOF
+    docker buildx build --push --platform=linux/arm64,linux/amd64 -t hubimage/builder-pypy:$line - << EOF
 FROM hubimage/runtime-pypy:$line
 RUN mkdir -p /builder && \
     pip config --user set global.index https://mirrors.aliyun.com/pypi/simple/ && \
--- runtime/pypy/run.sh
+++ runtime/pypy/run.sh
@@ -3,10 +3,11 @@
 for line in $(cat tag)
 do
     echo $line
-    docker buildx build --push --platform=linux/arm,linux/arm64,linux/amd64 -t hubimage/runtime-pypy:$line - << EOF
+    docker buildx build --push --platform=linux/arm64,linux/amd64 -t hubimage/runtime-pypy:$line - << EOF
 FROM pypy:$line
 LABEL maintainer="shaowenchen <mail@chenshaowen.com>"
 RUN mkdir -p /runtime && \
+    pip install --upgrade pip && \
     ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime && \
     echo "Asia/Shanghai" > /etc/timezone
 WORKDIR /runtime
